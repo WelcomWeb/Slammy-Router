66394f9290b7968da7ad141ae9261f4886ae1962
Björn Wikström <bjorn@welcom.se>
2015-12-09 12:52:23 +0100

    Adding tests for add/remove route during runtime

--- __tests__/slammy-router-test.js
+++ __tests__/slammy-router-test.js
@@ -163,4 +163,77 @@ describe('Slammy-Router', function () {
 		reference.setRoute("/test-route");
 		expect(mock.callback).not.toHaveBeenCalled()
 	});
+
+	it('allows addition of routes during runtime', function () {
+		var TestUtils = require('react-addons-test-utils'),
+			React = require('react'),
+			ReactDOM = require('react-dom'),
+			Router = require('../src/slammy-router');
+		
+		var routes = {
+			"/default": React.createClass({
+					render: function () {
+						return <div>default</div>;
+					}
+				})
+		};
+		
+		var reference = ReactDOM.render((<Router default="/default" routes={routes} notfound={jest.genMockFunction()} />), node);
+		var view;
+		
+		var mock = {
+			"callback": function () {}
+		};
+		spyOn(mock, "callback");
+		
+		Router.addRouteChangeListener(mock.callback);
+		
+		reference.setRoute("/default");
+		view = TestUtils.findRenderedDOMComponentWithTag(reference, 'div');
+		expect(view.textContent).toEqual("default");
+
+		Router.addRoute("/test-route", React.createClass({
+					render: function () {
+						return <div>test-route</div>;
+					}
+				}));
+		
+		reference.setRoute("/test-route");
+		view = TestUtils.findRenderedDOMComponentWithTag(reference, 'div');
+		expect(view.textContent).toEqual("test-route");
+	});
+
+	it('allows deletion of routes during runtime', function () {
+		var TestUtils = require('react-addons-test-utils'),
+			React = require('react'),
+			ReactDOM = require('react-dom'),
+			Router = require('../src/slammy-router');
+		
+		var routes = {
+			"/default": React.createClass({
+					render: function () {
+						return <div>default</div>;
+					}
+				}),
+			"/test-route": React.createClass({
+					render: function () {
+						return <div>test-route</div>;
+					}
+				})
+		};
+		
+		var notFoundRoute = React.createClass({
+			render: function () {
+				return <div>notfound</div>;
+			}
+		});
+		
+		var reference = ReactDOM.render((<Router default="/default" routes={routes} notfound={notFoundRoute} />), node);
+		
+		Router.removeRoute("/test-route");
+
+		reference.setRoute("/test-route");
+		var view = TestUtils.findRenderedDOMComponentWithTag(reference, 'div');
+		expect(view.textContent).toEqual("notfound");
+	});
 });
